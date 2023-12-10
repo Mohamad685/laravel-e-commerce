d6753e43cca760a7cece833c914e353c18941549
unknown <mohammad.fakih685@gmail.com>
2023-12-10 15:25:59 +0200

    transactions table and get method implemented

--- e-commerce-new/routes/api.php
+++ e-commerce-new/routes/api.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\LaravelController;
 use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\OrderItemsController;
+use App\Http\Controllers\TransactionsController;
+
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,5 +22,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/insert_product', [ProductsController::class, 'insert_product']);
 Route::post('/update_product', [ProductsController::class, 'update_product']);
+Route::post('/insert_product', [ProductsController::class, 'get_products']);
+Route::post('/update_product', [ProductsController::class, 'delete_product']);
 Route::post('/create_user', [UsersController::class, 'create_user']);
-Route::post('/add_quantity', [UsersController::class, 'add_quantity']);
+Route::post('/add_quantity', [OrderItemsController::class, 'add_quantity']);
+Route::post('/get_transactions', [TransactionsController::class, 'get_transactions']);
