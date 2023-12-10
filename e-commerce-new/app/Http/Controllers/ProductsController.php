5faf0516d0b0e685a1a676c7e3ec01bf12caf0fc
unknown <mohammad.fakih685@gmail.com>
2023-12-10 23:57:27 +0200

    validation for product implemented

--- e-commerce-new/app/Http/Controllers/ProductsController.php
+++ e-commerce-new/app/Http/Controllers/ProductsController.php
@@ -1,14 +1,24 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Auth;
 use App\Models\Products;
 use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
+
+    private function validation_logging(){
+        $user = Auth::user();
+        if (!$user) return false;
+        return $user-> user_type_id;
+    } 
+
+   
+
     public function insert_product(Request $req)
     {
+        if($this->validation_logging()){
         $products = new Products;
         $products->seller_id = $req->seller_id;
         $products->name = $req->name;
@@ -16,7 +26,9 @@ class ProductsController extends Controller
         $products->price = $req->price;
         $products->stock = $req->stock;
         $products->save();
-
+        }
+        
+        
     }
 
     public function update_product(Request $req)
